f741ab3247be559d79bbb9432490d9c861cd9ed5
Adela Molina chacon <amolina-@c1r9s1.42madrid.com>
2021-03-10 21:46:57 +0100

    day at 42

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,8 +6,57 @@
 /*   By: amolina- <amolina-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 20:45:24 by amolina-          #+#    #+#             */
-/*   Updated: 2021/02/16 20:45:31 by amolina-         ###   ########.fr       */
+/*   Updated: 2021/03/10 21:45:35 by amolina-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+size_t	ft_strlen(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	malloqueando(char **tab, char *str, char t, int a)
+{
+	int b;
+	int k;
+
+	b = 0;
+	k = 0;
+	while (a < ft_strlen(str))
+	{
+		while (str[a] != t && str[a] != '\0')
+		{
+			a++;
+		}
+		a++;
+		b++;
+	}
+	tab = (char **)malloc(b * sizeof(char *));
+	a = 0;
+	while (k < b)
+	{
+		while (str[a] != t)
+		{
+			a++;
+		}
+		tab[k] = (char *)malloc (a * sizeof(char) + 1);
+		a++;
+		k++;
+	}
+}
+
+char **ft_split(char const *s, char c)
+{
+	int		i;
+	char	**table;
+
+	i = 0;
+	malloqueando(table, s, c, i);
+}
--- Pruebas_proyectos/Pruebas_Libft/strtrim/main_strtrim.c
+++ Pruebas_proyectos/Pruebas_Libft/strtrim/main_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: amolina- <amolina-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 20:55:23 by amolina-          #+#    #+#             */
-/*   Updated: 2021/03/09 21:38:20 by amolina-         ###   ########.fr       */
+/*   Updated: 2021/03/10 20:57:42 by amolina-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ char	*ft_strtrim(char const *s1, char const *set);
 
 int		main()
 {
-	char base[20] = "ahsemucadelaahsemuc";
-	char quitar[10] = "ahsemuc";
+	char base[20] = "ahsemucadelahsemuc";
+	char quitar[8] = "ahsemuc";
 	printf("%s\n", ft_strtrim(base, quitar));
 	return (0);
 }
