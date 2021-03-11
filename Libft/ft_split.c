d2b892850d8e79e7d8b65862121806f46aa2ca6c
Adela Molina chacon <amolina-@c1r10s3.42madrid.com>
2021-03-11 21:30:18 +0100

    day at 42

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: amolina- <amolina-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 20:45:24 by amolina-          #+#    #+#             */
-/*   Updated: 2021/03/10 21:45:35 by amolina-         ###   ########.fr       */
+/*   Updated: 2021/03/11 21:28:47 by amolina-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,31 +32,55 @@ void	malloqueando(char **tab, char *str, char t, int a)
 	while (a < ft_strlen(str))
 	{
 		while (str[a] != t && str[a] != '\0')
-		{
 			a++;
-		}
 		a++;
 		b++;
 	}
 	tab = (char **)malloc(b * sizeof(char *));
 	a = 0;
-	while (k < b)
+	while (str[a] != '\0')
 	{
+		b = a;
 		while (str[a] != t)
-		{
 			a++;
-		}
-		tab[k] = (char *)malloc (a * sizeof(char) + 1);
+		tab[k] = (char *)malloc ((a - b) * sizeof(char) + 1);
 		a++;
 		k++;
 	}
 }
 
-char **ft_split(char const *s, char c)
+char	**to_table(char **tab, char *str, char t)
+{
+	int i;
+	int j;
+	int k;
+	
+	i = 0;
+	j = 0;
+	k = 0;
+	while (i < ft_strlen(str))
+	{
+		while (str[i] != t && str[i] != '\0')
+		{
+			tab[j][k] = str[i];
+			i++;
+			k++;
+		}
+		tab[j][k] = '\0';
+		i++;
+		j++;
+		k = 0;
+	}
+	return (tab);
+}
+
+char	**ft_split(char const *s, char c)
 {
 	int		i;
 	char	**table;
 
 	i = 0;
 	malloqueando(table, s, c, i);
+	to_table(table, s, c);
+	return (table);
 }
